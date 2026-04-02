f1df7aa1b73f9e75020933450b7c9519c6f963e1
SkyReed <itzskyreed@gmail.com>
2026-04-03 00:31:08 +0500

    Юзер дто

---
+++ Domain/Entities/UserAuthDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public record UserAuthDto
+{
+    public required uint Identifier { get; init; }
+    public required long TelegramId { get; init; }
+    public required string Name { get; init; }
+};
